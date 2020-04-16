da0b9c48e5d9b958f530ba5cf1e138025f9398d1
kevin zhuang <kevin7441@gmail.com>
2020-04-16 15:30:48 +1000

    refactor

--- helper/git_data.sh
+++ helper/git_data.sh
@@ -15,7 +15,8 @@ function get_commit() {
   /usr/bin/git --git-dir="${DOTBARE_DIR}" --work-tree="${DOTBARE_TREE}" \
   log --oneline --color=always --decorate=short \
     | fzf --no-multi --header="${header}" \
-      --preview "echo {} | awk '{print \$1}' \
+      --preview "echo {} \
+        | awk '{print \$1}' \
         | xargs -I __ /usr/bin/git --git-dir=${DOTBARE_DIR} --work-tree=${DOTBARE_TREE} \
           show --color=always  __" \
     | awk '{print $1}'
@@ -49,4 +50,66 @@ function get_branch() {
     | fzf --no-multi --header="${header}" \
       --preview="/usr/bin/git --git-dir=${DOTBARE_DIR} --work-tree=${DOTBARE_TREE} \
       log --oneline --graph --color=always --decorate=short {}"
+}
+
+#######################################
+# let user select a dotbare tracked file interactively
+# Arguments:
+#   $1: the helper message to display in the fzf header
+#   $2: if exist, don't do multi selection, do single
+# Outputs:
+#   the selected file path
+#   e.g.$HOME/.config/nvim/init.vim
+#######################################
+function get_git_file() {
+  local header="${1:-select a file}"
+  local no_multi="$2"
+  if [[ -z "${no_multi}" ]]; then
+    /usr/bin/git --git-dir="${DOTBARE_DIR}" --work-tree="${DOTBARE_TREE}" \
+    ls-files --full-name --directory "${DOTBARE_TREE}" \
+      | fzf --multi --header="${header}" \
+        --preview "head -50 ${DOTBARE_TREE}/{}" \
+      | awk -v home="${DOTBARE_TREE}" '{print home "/" $0}'
+  else
+    /usr/bin/git --git-dir="${DOTBARE_DIR}" --work-tree="${DOTBARE_TREE}" \
+    ls-files --full-name --directory "${DOTBARE_TREE}" \
+      | fzf --no-multi --header="${header}" \
+        --preview "head -50 ${DOTBARE_TREE}/{}" \
+      | awk -v home="${DOTBARE_TREE}" '{print home "/" $0}'
+  fi
+}
+
+#######################################
+# let user select a modified file interactively
+# Arguments:
+#   $1: the helper message to display in the fzf header
+#   $2: if exists, don't do multi selection, do single
+# Outputs:
+#   the selected file path
+#   e.g.$HOME/.config/nvim/init.vim
+#######################################
+function get_modified_file() {
+  local header="${1:-select a modified file}"
+  local no_multi="$2"
+  if [[ -z "${no_multi}" ]]; then
+    /usr/bin/git --git-dir="${DOTBARE_DIR}" --work-tree="${DOTBARE_TREE}" \
+    diff --name-status \
+      | awk '{print "\033[31m" $1 " " $2}' \
+      | fzf --multi --header="${header}" \
+        --preview "echo {} \
+          | awk '{print \$2}' \
+          | xargs -I __ /usr/bin/git --git-dir=${DOTBARE_DIR} --work-tree=${DOTBARE_TREE} \
+            diff --color=always ${DOTBARE_TREE}/__" \
+      | awk -v home="${DOTBARE_TREE}" '{print home "/" $2}'
+  else
+    /usr/bin/git --git-dir="${DOTBARE_DIR}" --work-tree="${DOTBARE_TREE}" \
+    diff --name-status \
+      | awk '{print "\033[31m" $1 " " $2}' \
+      | fzf --no-multi --header="${header}" \
+        --preview "echo {} \
+          | awk '{print \$2}' \
+          | xargs -I __ /usr/bin/git --git-dir=${DOTBARE_DIR} --work-tree=${DOTBARE_TREE} \
+            diff --color=always ${DOTBARE_TREE}/__" \
+      | awk -v home="${DOTBARE_TREE}" '{print home "/" $2}'
+  fi
 }
