05472cf5eb4fea29500fac9f34a23b638cc48ac0
kevin zhuang <kevin7441@gmail.com>
2020-05-11 09:41:14 +1000

    shellcheck script

---
+++ shellcheck.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# run shellcheck against all scripts
+
+set -e
+
+scripts=()
+while IFS=  read -r -d $'\0' line; do
+    scripts+=("$line")
+done < <(
+  find \
+    dotbare \
+    helper/* \
+    scripts/* \
+    shellcheck.sh \
+    -type f \
+    -print0
+  )
+
+shellcheck -e SC1090 "${scripts[@]}"
+
+exit $?
