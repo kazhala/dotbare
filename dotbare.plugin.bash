1b648d4c3f5ec5ae14b916837b626843f61ea8b8
kevin zhuang <kevin7441@gmail.com>
2020-06-06 10:31:06 +1000

    feat(complete:bash): set up completion for subcommands

--- dotbare.plugin.bash
+++ dotbare.plugin.bash
@@ -1,2 +1,24 @@
 mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [[ :$PATH: != *:"${mydir}":* ]] && export PATH="$PATH:${mydir}"
+
+_dotbare_completions()
+{
+  local IFS=$'\n' subcommands
+  if [[ "$COMP_CWORD" -eq "1" ]]; then
+    subcommands="$(./dotbare -h \
+      | awk '{
+          if ($0 ~ /^  f.*/) {
+            gsub(/^  /, "", $0)
+            gsub(/\t\t/, "    ", $0)
+            print $0
+          }
+        }')"
+
+    COMPREPLY=($(compgen -W "${subcommands}" -- "${COMP_WORDS[1]}"))
+    if [[ ${#COMPREPLY[*]} -eq 1 ]]; then
+      COMPREPLY=( ${COMPREPLY[0]%% *} )
+    fi
+  fi
+}
+
+complete -F _dotbare_completions dotbare
